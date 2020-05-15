02eb194a1d952f2b24bfcee091c4110f8b816fb6
Sudipta065 <58800767+Sudipta065@users.noreply.github.com>
2020-05-14 23:23:44 -0700

    quotes

---
+++ script.js
@@ -0,0 +1,19 @@
+let myFetch = fetch(
+  `https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json`
+);
+
+myFetch
+  .then((results) => {
+    console.log(results);
+    return results.json();
+  })
+  .then((obj) => {
+    function callQuotes() {
+      let randomQuotes = obj.quotes[Math.round(Math.random() * 100)];
+      document.querySelector('#text').innerHTML = randomQuotes.quote;
+      document.querySelector('#author').innerHTML = randomQuotes.author;
+    }
+    callQuotes();
+
+    document.getElementById('button').addEventListener('click', callQuotes);
+  });
